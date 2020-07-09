8f5de1c787f80aee663be13a55e181b028274ebd
Aditya Goyal <adityagoyaldoon@gmail.com>
2020-07-09 15:54:35 +0530

    Adding gitignore

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/adityagoyal/Documents/Harvard Web Development Course/wiki/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
