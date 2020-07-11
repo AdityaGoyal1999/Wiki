5b569f36719591479da7f2589d6f7b6d5c758108
Aditya Goyal <adityagoyaldoon@gmail.com>
2020-07-11 12:25:48 +0530

    Django app connected and working

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name='ency'
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("Page/<str:title>/", views.singlePage, name="singlePage"),
+    path('search/', views.search, name="search"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 from . import util
 
+# print(util.list_entries(), "\n\n\n")
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def singlePage(request, title):
+
+    response = util.get_entry(title)
+    if response is None:
+        return HttpResponse("Nothing found")
+    else:
+        return HttpResponse(response)
+
+def search(request):
+
+    entry = request.GET['q']
+    response = util.get_entry(entry)
+    # print(response, "\n\n")
+    if response is not None:
+        return HttpResponse(response)
+    results = []
+    queries = util.list_entries()
+    print(queries, "\n\n\n")
+    for query in queries:
+        if entry.lower() in query.lower():
+            results.append(query)
+    return render(request, 'encyclopedia/searchResults.html', {"entry": entry, "results": results})
