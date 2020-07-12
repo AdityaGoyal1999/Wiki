45b817add2555b16e883bd84fe5d4d20ed32f2b3
Aditya Goyal <adityagoyaldoon@gmail.com>
2020-07-12 20:27:41 +0530

    Added tests

--- encyclopedia/tests.py
+++ encyclopedia/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+
+class FrontEndTests(TestCase):
+
+    # def setUp(self):
+
+    def test_is_working(self):
+        c = Client()
+        response = c.get("/wiki/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_enteries_not_empty(self):
+        c = Client()
+        response = c.get("/wiki/")
+        self.assertNotEqual(response.context['entries'], [])
+
+    # For python page testing
+    def test_python_page_working(self):
+        c = Client()
+        response = c.get("/wiki/Python/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_python_page_contents(self):
+        c = Client()
+        response = c.get("/wiki/Python/")
+        self.assertNotEqual(response.context['contents'], '')
+
+    
